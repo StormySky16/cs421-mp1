e8207aa6193bbe64b7600bda18a1726107c333ef
StormySky16 <stormysky128@gmail.com>
2024-06-02 22:02:43 -0500

    Finished MP1

--- mp1a-recursion/src/Lib.hs
+++ mp1a-recursion/src/Lib.hs
@@ -3,6 +3,10 @@
 
 --- Relevant Files
 --- --------------
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use map" #-}
+{-# HLINT ignore "Use foldr" #-}
+{-# HLINT ignore "Use :" #-}
 
 module Lib where
 
@@ -27,57 +31,107 @@ import qualified Prelude as P
 --- ### mytake
 
 -- don't forget to put the type declaration or you will lose points!
-mytake = undefined
+mytake :: Int -> [a] -> [a]
+mytake n [] = []
+mytake n (x:xs)
+    | n <= 0 = []
+    | otherwise = x : mytake (n-1) xs
+    --syntax for if-else equivalent in Haskell found on Stack Overflow
 
 --- ### mydrop
 
 -- don't forget to put the type declaration or you will lose points!
-mydrop = undefined
+mydrop :: Int -> [a] -> [a]
+mydrop n [] = []
+mydrop n (x:xs)
+    | n <= 0 = x:xs
+    | n == 1 = xs
+    | otherwise = mydrop (n-1) xs
+
+
+
 
 --- ### rev
 
 -- don't forget to put the type declaration or you will lose points!
-rev = undefined
+rev :: [a] -> [a]
+rev [] = []
+rev inputList = revHelper inputList []
+    where
+        revHelper :: [a] -> [a] -> [a]
+        revHelper [] temp = temp --empty inputList, nothing left to put into temp
+        revHelper (x:xs) temp = revHelper xs (x:temp) {-take head of current input, append to front of temp,
+                                                        recursively repeat until entire list is reversed.-}
+    --completed with help from ChatGPT for better understanding of Haskell prepending syntax and inspiration of methodology from StackOverflow
+
 
 --- ### app
 
 -- don't forget to put the type declaration or you will lose points!
-app = undefined
-
+-- x: list to append, y: destination list for the append
+-- appends to front of list
+app :: [a] -> [a] -> [a]
+app [] y = y
+app (x:xs) y = x : app xs y
 --- ### inclist
 
 -- don't forget to put the type declaration or you will lose points!
-inclist = undefined
-
+inclist :: Num a => [a] -> [a]
+inclist [] = []
+inclist (x:xs) = x + 1 : inclist xs
 --- ### sumlist
 
 -- don't forget to put the type declaration or you will lose points!
-sumlist = undefined
+sumlist :: Num a => [a] -> a
+sumlist [] = 0
+sumlist list = sumlistHelper list 0
+    where
+        sumlistHelper :: Num a => [a] -> a -> a
+        sumlistHelper [] sum = sum
+        sumlistHelper (x:xs) sum = sumlistHelper xs (x+sum)
 
 --- ### myzip
 
 -- don't forget to put the type declaration or you will lose points!
-myzip = undefined
-
+myzip :: [a] -> [b] -> [(a,b)]
+--myzip [] b = []
+--myzip a [] = []
+myzip a b = myzipHelper a b []
+    where
+        myzipHelper :: [a] -> [b] -> [(a,b)] -> [(a,b)]
+        myzipHelper [] b zipped = zipped
+        myzipHelper a [] zipped = zipped
+        myzipHelper (x:xs) (y:ys) zipped =  (x,y): myzipHelper xs ys zipped
+    -- assisted by ChatGPT in regards to how to use pairs
 --- ### addpairs
 
 -- don't forget to put the type declaration or you will lose points!
-addpairs = undefined
+addpairs :: (Num a) => [a] -> [a] -> [a]
+addpairs a b = addpairsHelper (myzip a b)
+    where
+        addpairsHelper :: (Num a) => [(a,a)] -> [a]
+        addpairsHelper [] = []
+        addpairsHelper (x:xs) = (fst x + snd x) : addpairsHelper xs
+    -- assisted by ChatGPT to find how to access values of a pair
+
 
 --- ### ones
 
 -- don't forget to put the type declaration or you will lose points!
-ones = undefined
+ones :: [Integer]
+ones = 1 : ones
 
 --- ### nats
 
 -- don't forget to put the type declaration or you will lose points!
-nats = undefined
+nats :: [Integer]
+nats = [0..]
 
 --- ### fib
 
 -- don't forget to put the type declaration or you will lose points!
-fib = undefined
+fib :: [Integer]
+fib = [0,1] P.++ addpairs fib (tail fib)
 
 --- Set Theory
 --- ----------
@@ -85,22 +139,46 @@ fib = undefined
 --- ### add
 
 -- don't forget to put the type declaration or you will lose points!
-add = undefined
+add :: Ord a => a -> [a] -> [a]
+add n [] = [n]
+add n (x:xs)
+    | n < x = n:(x:xs)
+    | n > x = x: add n xs
+    | otherwise = x:xs
 
 --- ### union
 
 -- don't forget to put the type declaration or you will lose points!
-union = undefined
+union :: Ord a => [a] -> [a] -> [a]
+union [] b = b
+union a [] = a
+union (x:xs) (y:ys)
+    | x == y = union xs (y:ys)
+    | x < y = x : union xs (y:ys)
+    | otherwise = y: union (x:xs) ys
+
+
+
 
 --- ### intersect
 
 -- don't forget to put the type declaration or you will lose points!
-intersect = undefined
+intersect :: Ord a => [a] -> [a] -> [a]
+intersect [] b = []
+intersect a [] = []
+intersect (x:xs) (y:ys)
+    | x < y = intersect xs (y:ys)
+    | x > y = intersect (x:xs) ys
+    | otherwise = x : intersect xs ys
 
 --- ### powerset
 
 -- don't forget to put the type declaration or you will lose points!
-powerset = undefined
+powerset :: Ord a => [a] -> [[a]]
+powerset [] = [[]]
+powerset (x:xs) =  union (powerset xs) (P.map (x:) (powerset xs))
+    -- detailed explanation of powersets and ways to implement algorithms to calculate one assisted by ChatGPT
+    -- applications of map function found from documentation on zvon.org
 
 --- Higher Order Functions
 --- ----------------------
@@ -108,9 +186,13 @@ powerset = undefined
 --- ### inclist'
 
 -- don't forget to put the type declaration or you will lose points!
-inclist' = undefined
+inclist' :: Num a => [a] -> [a]
+inclist' = P.map (+1)
+    -- originally inclist' list = P.map(+1) list, VS Code suggestion simplified to current state
 
 --- ### sumlist'
 
 -- don't forget to put the type declaration or you will lose points!
-sumlist' = undefined
+sumlist' :: (Num a) => [a] -> a
+sumlist' = P.sum
+    -- details of sum function found from documentation on zvon.org
--- mp1b-adts/src/Lib.hs
+++ mp1b-adts/src/Lib.hs
@@ -3,6 +3,8 @@
 
 --- Relevant Files
 --- --------------
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
 
 module Lib where
 
@@ -25,37 +27,74 @@ data Exp = IntExp Integer
 --- ### list2cons
 
 -- don't forget to put the type declaration or you will lose points!
-list2cons = undefined
+list2cons :: [a] -> List a
+list2cons [] = Nil
+list2cons (x:xs) = Cons x (list2cons xs)
 
 --- ### cons2list
 
 -- don't forget to put the type declaration or you will lose points!
-cons2list = undefined
+cons2list :: List a -> [a]
+cons2list Nil = []
+cons2list (Cons x xs) = x: cons2list xs
 
 --- ### eval
 
 -- don't forget to put the type declaration or you will lose points!
-eval = undefined
+eval :: Exp -> Integer
+eval (IntExp x) = x
+eval (PlusExp exp) = sum (addHelper exp [])
+    where
+        addHelper :: [Exp] -> [Integer] -> [Integer]
+        addHelper [] list = list
+        addHelper (x:xs) list = eval x : addHelper xs list
+eval (MultExp exp) = product (multHelper exp [])
+    -- product function suggested by VS Code, originally used foldr (*) 1
+    -- details on how foldr worked assisted by ChatGPT and cross referenced in documentation
+    where
+        multHelper :: [Exp] -> [Integer] -> [Integer]
+        multHelper [] list = list
+        multHelper (x:xs) list = eval x : multHelper xs list
+
+
 
 --- ### list2cons'
 
 -- don't forget to put the type declaration or you will lose points!
-list2cons' = undefined
+list2cons' :: [a] -> List a
+list2cons' = foldr Cons Nil -- foldr using Cons as binary function and Nil as the initial accumulator value
 
 --- ### BinTree
 
 -- BinTree
+data BinTree a =  Node a (BinTree a) (BinTree a)
+                | Leaf
+    deriving (Show)
+
+
+    -- created with assistance from ChatGPT, modeled after custom List 
 
 --- ### sumTree
 
 -- don't forget to put the type declaration or you will lose points!
-sumTree = undefined
+
+sumTree :: Num a => BinTree a -> a
+sumTree Leaf = 0
+sumTree (Node val left right) = val + sumTree left + sumTree right
 
 --- ### SimpVal
 
 -- SimpVal
-
+data SimpVal = IntVal Integer
+            | BoolVal Bool
+            | StrVal String
+            | ExnVal String
+    deriving (Show)
+    --Modeled after Exp ADT
 --- ### liftIntOp
 
 -- don't forget to put the type declaration or you will lose points!
-liftIntOp = undefined
+
+liftIntOp :: (Integer -> Integer -> Integer) -> SimpVal -> SimpVal -> SimpVal
+liftIntOp op (IntVal x) (IntVal y) = IntVal(op x y) -- Assisted by ChatGPT to better understand how the operator worked
+liftIntOp _ _ _ = ExnVal("not an IntVal!")
